2f25ccd4d9e6507f4b984a81fb583eba19d824c3
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-02-27 13:14:09 -0300

    Verificar a quantidade de elementos no vetor e Imprimir elementos do vetor

---
+++ src/main/java/com/estruturadedados/arrays/teste/teste03.java
@@ -0,0 +1,29 @@
+package com.estruturadedados.arrays.teste;
+
+import com.estruturadedados.arrays.Vetor;
+
+public class teste03 {
+
+    public static void main(String[] args) {
+         /*
+
+         Verificar tamanho e imprimir elementos do vetor
+          */
+
+        Vetor vetor = new Vetor(10);
+
+        vetor.adiciona("Elemento 1");
+        vetor.adiciona("Elemento 2");
+        vetor.adiciona("Elemento 3");
+
+        System.out.println(vetor.tamanho());
+
+        System.out.println(vetor.toString());
+
+
+
+
+
+    }
+
+}
