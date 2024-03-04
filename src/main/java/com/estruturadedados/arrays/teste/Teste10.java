55a9bdc52b9fea74a1adf09f289ec9b35107c2d3
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-03-03 21:30:22 -0300

    Configurando tipos genericos

---
+++ src/main/java/com/estruturadedados/arrays/teste/Teste10.java
@@ -0,0 +1,14 @@
+package com.estruturadedados.arrays.teste;
+
+import com.estruturadedados.arrays.Lista;
+
+public class Teste10 {
+
+    public static void main(String[] args) {
+
+        Lista vetor = new Lista<String>(1);
+
+
+
+    }
+}
