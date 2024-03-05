55e80bd7ee159d69eafbc285aa05641eb154fcc3
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-03-04 22:58:02 -0300

    Exerc 01 método contains

--- src/main/java/com/estruturadedados/arrays/Lista.java
+++ src/main/java/com/estruturadedados/arrays/Lista.java
@@ -72,6 +72,12 @@ public class Lista<T> {
         return -1;
     }
 
+    public boolean contem(T elemento) {
+        return busca(elemento) > -1;
+    }
+
+
+
     // B G D E F -> posição a ser removida é 1 (G)
     // 0 1 2 3 4 -> tamanho é 5
     // vetor[1] = vetor[2]
